4620bf8489d055080fa9303758f479044fc1dd67
frcdev <frcdev@FRC2607DEV.ACADEMIC.EDU>
2015-02-03 21:18:45 -0500

    Practice Bot

--- PracticeBot/src/org/usfirst/frc/team2607/robot/Robot.java
+++ PracticeBot/src/org/usfirst/frc/team2607/robot/Robot.java
@@ -1,14 +1,16 @@
 
 package org.usfirst.frc.team2607.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Gyro;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -19,7 +21,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Robot extends IterativeRobot {
 	
-	Joystick sticktoriaJustice; 
+	robovikingStick sticktoriaJustice; 
 	Compressor comPreston; 
 	Talon FrontL;
 	Talon FrontR;
@@ -31,12 +33,8 @@ public class Robot extends IterativeRobot {
 	SmoothedEncoder encFL, encFR, encBL, encBR;
 	SmoothedEncoder encVator;
 	Gyro gyroPyro;
-	SmartDashboard iDash5s;
 	double x, y, z;
-	double lift = .5;
-	double lower = -.5;
-	double[] driveValerie = new double[3];
-	double[] DeadZones = new double[]{0.25, 0.25, 0.25};
+	boolean Saul;
 	/**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
@@ -48,8 +46,8 @@ public class Robot extends IterativeRobot {
     	FrontR = new Talon(Constants.talonFrontRight);
     	BackL = new Talon(Constants.talonBackLeft);
     	BackR = new Talon(Constants.talonBackRight);
-    	Hellovator1 = new Talon(Constants.Elevator1);
-    	Hellovator2 = new Talon(Constants.Elevator2);
+    	Hellovator1 = new Talon(Constants.talonElevator1);
+    	Hellovator2 = new Talon(Constants.talonElevator2);
     	Saulenoid = new Solenoid(Constants.solenoidChannel);
     	encFR = new SmoothedEncoder(0, 1, false, Encoder.EncodingType.k1X);
     	encFL = new SmoothedEncoder(2, 3, false, Encoder.EncodingType.k1X);
@@ -57,10 +55,10 @@ public class Robot extends IterativeRobot {
     	encBL = new SmoothedEncoder(6, 7, false, Encoder.EncodingType.k1X);
     	encVator = new SmoothedEncoder(8, 9, false, Encoder.EncodingType.k1X);
     	DriveRobot = new RobotDrive(FrontL, BackL, FrontR, BackR);
-    	DriveRobot.setInvertedMotor(FrontL, true);
-    	DriveRobot.setInvertedMotor(BackL, true);
+    	DriveRobot.setInvertedMotor(MotorType.kFrontLeft, true);
+    	DriveRobot.setInvertedMotor(MotorType.kRearLeft, true);
     	gyroPyro = new Gyro(Constants.gyroChannel);
-    	iDash5s = new SmartDashboard
+    	
     	comPreston.start();
     }
 
@@ -75,41 +73,12 @@ public class Robot extends IterativeRobot {
      * This function is called periodically during operator control
      */
     public void teleopPeriodic() {
-    	DriveRobot.mecanumDrive_Cartesian(driveValerie[0], driveValerie[1], driveValerie[2], 0);
+    	DriveRobot.mecanumDrive_Cartesian(sticktoriaJustice.getX(), sticktoriaJustice.getY(), -sticktoriaJustice.getRawAxis(4), 0);
+
     	
-    	driveValerie[0] = sticktoriaJustice.getX();
-    	driveValerie[1] = sticktoriaJustice.getY();
-    	driveValerie[2] = -(sticktoriaJustice.getRawAxis(4));
     	
-    	for (int i = 0; i < 2; i++) {
-    		if (Math.abs(driveValerie[i]) <= deadZones[i]) {
-    			driveValerie[i] = 0;
-    		}
-    		if (driveValerie[i] > deadZones[i] && driveValerie[i] <= deadZones[i] * 2) {
-    			driveValerie[i] = (driveValerie[i] - .15) * 2;
-    		}
-    		if (driveValerie[i] < -deadZones[i] >= driveValerie[i] >= -2 * deadZones[i]) {
-    			driveValerie[i] = (driveValerie)[i] + .15) * 2;
-    		}
-    		DriveRobot.mecanumDrive_Cartesian(sticktoriaJustice.getX(), sticktoriaJustice.getY(), -sticktoriaJustice.getRawAxis(4), 0);
-    	
-    	if(sticktoriaJustice.getRawButton(4)){
-    		
-    		Hellovator1.set(lift);
-    		Hellovator2.set(lift);
-    		
-    	}else if(sticktoriaJustice.getRawButton(1)){
-    		
-    		Hellovator1.set(lower);
-    		Hellovator2.set(lower);
-    	}
     	Saulenoid.set(sticktoriaJustice.getToggleButton(2));
 
-    	iDash5s.putNumber("Front Right Rate ", encFR.getRate());
-    	iDash5s.putNumber("Front Left Rate ", encFL.getRate());
-    	iDash5s.putNumber("Back Right Rate ", encBR.getRate());
-    	iDash5s.putNumber("Back Left Rate ", encBL.getRate());
-    	iDash5s.getNumber("Vator Rate ", encVator.getRate());
     }
     
     /**
@@ -118,25 +87,19 @@ public class Robot extends IterativeRobot {
     public void testPeriodic() {
     	
 
-    	iDash5s.putNumber("Front Right Rate ", encFR.getRate());
-    	iDash5s.putNumber("Front Left Rate ", encFL.getRate());
-    	iDash5s.putNumber("Back Right Rate ", encBR.getRate());
-    	iDash5s.putNumber("Back Left Rate ", encBL.getRate());
-    	iDash5s.getNumber("Vator Rate ", encVator.getRate());
-    	
-        if (xboxSpeedRacer.getRawButton(4)) {
+        if (sticktoriaJustice.getRawButton(4)) {
             y = -.5;
             x = 0.0;
             z = 0.0;
-        } else if (xboxSpeedRacer.getRawButton(1)) {
+        } else if (sticktoriaJustice.getRawButton(1)) {
             y = .5;
             x = 0.0;
             z = 0.0;
-        } else if (xboxSpeedRacer.getRawButton(3)) {
+        } else if (sticktoriaJustice.getRawButton(3)) {
             y = 0.0;
             x = -.5;
             z = 0.0;
-        } else if (xboxSpeedRacer.getRawButton(2)) {
+        } else if (sticktoriaJustice.getRawButton(2)) {
             y = 0.0;
             x = .5;
             z = 0.0;
@@ -152,4 +115,4 @@ public class Robot extends IterativeRobot {
     
     }
     
-}
+
