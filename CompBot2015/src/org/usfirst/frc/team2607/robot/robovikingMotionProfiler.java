3b25db127fc365d1104d368893d7f67da7eaa930
tHartman77 <20151200@archwood.org>
2015-03-15 17:45:17 -0400

    Added auton motion profiles for photoeyes just in case

--- CompBot2015/src/org/usfirst/frc/team2607/robot/robovikingMotionProfiler.java
+++ CompBot2015/src/org/usfirst/frc/team2607/robot/robovikingMotionProfiler.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import com.kauailabs.nav6.frc.IMUAdvanced;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Ultrasonic;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -11,6 +12,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class robovikingMotionProfiler implements Runnable{
 	
 	robovikingMecanumDrive drive;
+	DigitalInput autoEye;
 	
 	boolean running = false;
 	
@@ -30,12 +32,23 @@ public class robovikingMotionProfiler implements Runnable{
 	double dgAcceptableRange = 5;
 	IMUAdvanced navX;
 	
+	Vector<Double> deDirection = null;
+	boolean eyeTrigger;
+	
+	Vector<Double> dreDirection = null;
+	
 	private double ftbCorrection = Constants.ftbCorrectionNoTote;
 	
 	
 	public robovikingMotionProfiler(robovikingMecanumDrive someDrive){
 		drive = someDrive;
 		navX = drive.getnavX();
+	}
+	
+	public robovikingMotionProfiler(robovikingMecanumDrive someDrive, DigitalInput eye){
+		drive = someDrive;
+		navX = drive.getnavX();
+		autoEye = eye;
 	}
 	
 	private void drivePathCode (int[][] steps) throws InterruptedException{
@@ -141,6 +154,39 @@ public class robovikingMotionProfiler implements Runnable{
 		}
 	}
 	
+	public void driveUntilTriggerCode() throws InterruptedException{
+		long startTime = System.currentTimeMillis();
+		
+		drive.resetDistance();
+		
+		while (System.currentTimeMillis() < startTime + 10000){
+			
+			if (autoEye.get() != eyeTrigger){
+				drive.correctedMecanumDrive(0, 0, -dreDirection.get(0), 0, ftbCorrection);
+			} else {
+				break;
+			}
+			
+			
+			Thread.sleep(3);
+				
+		}
+		
+	}
+	
+	public void rotateUntilTriggerCode() throws InterruptedException{
+		long startTime = System.currentTimeMillis();
+		
+		while (System.currentTimeMillis() < startTime + 5000){
+			if (autoEye.get() != eyeTrigger){
+				drive.correctedMecanumDrive(-deDirection.get(0), -deDirection.get(1), 0, 0, ftbCorrection);
+			} else {
+				break;
+			}
+			Thread.sleep(3);
+		}
+	}
+	
 	
 
 	@Override
@@ -172,6 +218,12 @@ public class robovikingMotionProfiler implements Runnable{
 				if (dgDegree > 0){
 					rotateUntilDegreeCode();
 					dgDegree = 0;
+					running = false;
+				}
+				
+				if (deDirection != null){
+					driveUntilTriggerCode();
+					deDirection = null;
 					running = false;
 				}
 
@@ -250,6 +302,34 @@ public class robovikingMotionProfiler implements Runnable{
 			}
 		}
 		
+	}
+	
+	public void driveUntilEyeTrigger(Vector<Double> motion, boolean trigger, boolean thread){
+		running = true;
+		deDirection = motion;
+		eyeTrigger = trigger;
+		if (!thread){
+			try {
+				driveUntilTriggerCode();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void rotateUntilEyeTrigger(Vector<Double> motion, boolean trigger, boolean thread){
+		running = true;
+		dreDirection = motion;
+		eyeTrigger = trigger;
+		if (!thread){
+			try {
+				rotateUntilTriggerCode();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public void setFtB(double d){
